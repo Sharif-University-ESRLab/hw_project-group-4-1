81fc5d3f0be84a1abc89b8ee2f3a253fd120a92b
SmsS4 <smss.lite@gmail.com>
2022-12-26 15:55:28 +0330

    Add quick

--- Code/server/server/runner.sh
+++ Code/server/server/runner.sh
@@ -5,3 +5,4 @@ python -m http.server
 python -m uploadserver
 curl -X POST http://127.0.0.1:8000/upload -F 'files=@/home/smss/Downloads/108585262.jpeg'
 
+python server/quic/http3_client.py --ca-certs cert.pem https://localhost:4433/10000000
