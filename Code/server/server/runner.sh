f478e4a4ac5cdb39aa8521fda7e1d20a3b9d5f8c
SmsS4 <smss.lite@gmail.com>
2022-12-26 15:59:36 +0330

    Refactor

--- Code/server/server/runner.sh
+++
@@ -1,8 +0,0 @@
-# Download
-python -m http.server
-
-# Upload
-python -m uploadserver
-curl -X POST http://127.0.0.1:8000/upload -F 'files=@/home/smss/Downloads/108585262.jpeg'
-
-python server/quic/http3_client.py --ca-certs cert.pem https://localhost:4433/10000000
