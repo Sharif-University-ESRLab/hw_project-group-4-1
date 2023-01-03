0b6cfa2ed1cae6a1f630a3186d709e0c082bf1f0
SmsS4 <smss.lite@gmail.com>
2023-01-03 19:13:29 +0330

    Add more docs

--- Code/server/server/tcp.py
+++ Code/server/server/tcp.py
@@ -1,6 +1,5 @@
 import datetime
 import socket
-from typing import List
 
 from glogger.logger import get_logger
 
@@ -15,15 +14,14 @@ def start(port: int):
         port: socket port
     """
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(('0.0.0.0', port))
-
-        s.listen()
+        s.bind(('0.0.0.0', port))  # Bind to 0.0.0.0:port
+        s.listen()  # Waits for new client to connect
         conn, addr = s.accept()
         logger.info("New connecting %s", addr)
         return conn
 
 
-def start_throughput(port: int, period: int, upload: bool, packet_size: int) -> List[int]:
+def start_throughput(port: int, period: int, upload: bool, packet_size: int) -> None:
     """
     Start a tcp socket server to test throughput
 
@@ -35,24 +33,25 @@ def start_throughput(port: int, period: int, upload: bool, packet_size: int) -> 
     """
     conn = start(port)
     logger.info("Start tcp throughput with period: %d upload: %s", period, upload)
-    now = datetime.datetime.now()
-    bytes_cnt = [0] * period
+    now = datetime.datetime.now()  # test start time
+    bytes_cnt = [0] * period  # Result of number of bytes sent or received per seconds
     while True:
         total = (datetime.datetime.now() - now).total_seconds()
         if total >= period:
             break
         if upload:
+            # Receive `packet_size` bytes of data
             data = conn.recv(packet_size)
             bytes_cnt[int(total)] += len(data)
         else:
-            conn.sendall(b'*'*packet_size)
+            # Sends `packet_size` bytes of data
+            conn.sendall(b'*' * packet_size)
             bytes_cnt[int(total)] += packet_size
     logger.info("Result: %s", bytes_cnt)
-    conn.close()
-    return bytes_cnt
+    conn.close()  # Close connection
 
 
-def start_latency(port: int, number_of_packets: int) -> List[int]:
+def start_latency(port: int, number_of_packets: int) -> None:
     """
     Starts latency test
 
@@ -62,12 +61,12 @@ def start_latency(port: int, number_of_packets: int) -> List[int]:
     """
     conn = start(port)
     logger.info("Start tcp latency with %d packets", number_of_packets)
-    result = []
+    result = []  # Result of latency tests
     for i in range(number_of_packets):
+        # Sends 1 byte of data and wait for 1 byte of data to receive
         now = datetime.datetime.now()
         conn.sendall(b'$')
         conn.recv(1)
         result.append(int((datetime.datetime.now() - now).total_seconds() * 1_000))
-    conn.close()
+    conn.close()  # Close connection
     logger.info("Result: %s", result)
-    return result
