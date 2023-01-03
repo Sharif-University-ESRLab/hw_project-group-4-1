9293e35b9de852dfff45cd701cc78cd494fefb7b
SmsS4 <smss.lite@gmail.com>
2023-01-03 19:21:07 +0330

    Add doc

--- Code/server/server/udp.py
+++ Code/server/server/udp.py
@@ -1,13 +1,13 @@
 import datetime
 import socket
-from typing import List
+from typing import Tuple
 
 from glogger.logger import get_logger
 
 logger = get_logger('udp')
 
 
-def start(port: int):
+def start(port: int) -> Tuple[socket.socket, Tuple[str, int]]:
     """
     Starts a udp socket on given port
 
@@ -16,7 +16,8 @@ def start(port: int):
     """
     logger.info("Waiting for udp socket")
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(('0.0.0.0', port))
+    s.bind(('0.0.0.0', port))  # Bind to 0.0.0.0:port
+    # Waits for new client to connect and send 1 byte of data
     while True:
         data, addr = s.recvfrom(1)
         if data is not None:
@@ -25,7 +26,7 @@ def start(port: int):
     return s, (addr[0], 10210)
 
 
-def start_throughput(port: int, period: int, upload: bool, packet_size: int):
+def start_throughput(port: int, period: int, upload: bool, packet_size: int) -> None:
     """
     Start an udp socket server to test throughput
 
@@ -37,22 +38,23 @@ def start_throughput(port: int, period: int, upload: bool, packet_size: int):
     """
 
     conn, addr = start(port)
-    logger.info("Start udp throughput with period: %d, addr: %s", period,  addr)
-    now = datetime.datetime.now()
-    bytes_cnt = [0] * period
+    logger.info("Start udp throughput with period: %d, addr: %s", period, addr)
+    now = datetime.datetime.now()  # test start time
+    bytes_cnt = [0] * period  # Result of number of bytes sent or received per seconds
     while True:
         total = (datetime.datetime.now() - now).total_seconds()
         if total >= period:
             break
         if upload:
+            # Receive `packet_size` bytes of data
             data, _ = conn.recvfrom(packet_size)
             bytes_cnt[int(total)] += len(data)
         else:
-            conn.sendto(b'*'*packet_size, addr)
+            # Sends `packet_size` bytes of data
+            conn.sendto(b'*' * packet_size, addr)
             bytes_cnt[int(total)] += packet_size
     logger.info("Result: %s", bytes_cnt)
-    conn.close()
-    return bytes_cnt
+    conn.close()  # Close connection
 
 
 def start_latency(port: int, number_of_packets: int) -> List[int]:
@@ -65,12 +67,12 @@ def start_latency(port: int, number_of_packets: int) -> List[int]:
     """
     conn, addr = start(port)
     logger.info("Start udp latency with %d packets", number_of_packets)
-    result = []
+    result = []  # Result of latency tests
     for i in range(number_of_packets):
+        # Sends 1 byte of data and wait for 1 byte of data to receive
         now = datetime.datetime.now()
         conn.sendto(b'$', addr)
         conn.recv(1)
         result.append(int((datetime.datetime.now() - now).total_seconds() * 1_000))
-    conn.close()
+    conn.close()  # Close connection
     logger.info("Result: %s", result)
-    return result
